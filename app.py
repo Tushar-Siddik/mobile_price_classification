7f39dbfa650f2bc44d24f945e75085b2076bdbd9
Tushar Siddik <55056869+Tushar-Siddik@users.noreply.github.com>
2024-04-16 00:42:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 
 def main():
     # st.title('Mobile Price Classification')
-    st.header('Mobile Price Classification')
+    # st.header('Mobile Price Classification')
     html_temp = """
     <div style="background:#025246 ;padding:10px">
     <h2 style="color:white;text-align:center;">Mobile Price Classification App</h2>
